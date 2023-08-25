147fea14e36be66a57f84a7067ada42516ef809d
mayefa10 <mayefa10@yahoo.com>
2023-08-24 19:07:06 -0500

    create database connection with mongoDb and create the functions update, get, delete and put

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,47 @@
-const errorHandler = (err,req,res,next)=>{
-    const statusCode = res.statusCode ? res.statusCode : 500;
-    res.json({message: err.message, stackTrace: err.stack})
-}
+const { constants } = require('../constants');
 
-module.exports =errorHandler;
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: 'Validation Failed',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+
+      break;
+    case constants.NOT_FOUND:
+      res.json({
+        title: 'Not found',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: 'Unauthorized',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+
+    case constants.FORBIDDEN:
+      res.json({
+        title: 'Forbidden',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+
+      case constants.SERVER_ERROR:
+      res.json({
+        title: 'server error',
+        message: err.message,
+        stackTrace: err.stack,
+      });
+    default:
+        console.log("successful connection");
+      break;
+  }
+};
+
+module.exports = errorHandler;
